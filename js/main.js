c2c6e88c281955202f85f29875140717b97b5e81
jona <jona@jona.com>
2020-05-07 09:14:42 +0300

    fixed nav bar responsiveness

---
+++ js/main.js
@@ -0,0 +1,23 @@
+const menu_button = document.querySelector(".menu");
+const overlay = document.querySelector(".overlay");
+const show_case = document.querySelector("header");
+const body = document.querySelector("body");
+
+const menu_close = document.querySelector(".menu-close");
+
+menu_button.addEventListener("click", () => {
+  console.log("Hello");
+  overlay.style.display = "block";
+  overlay.style.background = "darkslategray";
+  overlay.style.height = "100%";
+  overlay.style.display = "flex";
+  overlay.style.justifyContent = "center";
+  overlay.style.alignItems = "center";
+  overlay.style.flexDirection = "column";
+  body.style.position = "fixed";
+  show_case.style.width = "100%";
+});
+
+menu_close.addEventListener("click", () => {
+  overlay.style.display = "none";
+});
